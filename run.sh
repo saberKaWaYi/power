ac611837c13c631ade5d69f27517e6ba6ebaf6f9
hzx <hzx@hzxdeMac-mini.local>
2025-09-12 10:56:46 +0800

    feat: support k8s

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+
+service cron start
+
+web(){
+    touch /var/log/web.log
+    nohup gunicorn -w 16 settings.wsgi:application -b 0.0.0.0:8000 -t 600  --timeout 600 >> /var/log/web.log 2>&1 &
+    tail -f /var/log/web.log
+}
+
+case "$1" in
+    web)
+        web
+        ;;
+    *)
+        cat <<EOF
+选项:
+    web) web
+    *) --help
+EOF
+        ;;
+esac
+
