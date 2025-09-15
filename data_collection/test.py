ead280cb99706f3ce48b035d9b74060f4bab9590
LCL <1921699889@qq.com>
2025-09-15 11:58:30 +0800

    修改测试用例

--- data_collection/test.py
+++ data_collection/test.py
@@ -21,11 +21,23 @@ import requests
 if __name__=="__main__":
     url_post='http://10.213.136.111:40061/network_app/distribute_config/exec_cmd/'
     config={
-        "device_hostname":"CNIQN-POD235-F69-CL-17",
+        "device_hostname":"CNIQN-POD235-F69-CL-19",
         "operator":"devops",
         "is_edit":False,
         "cmd":""
     }
+    url_get=f"http://10.216.142.10:40061/network_app/conf/device/data_list/?device_hostname={hostname}"
+    response_url_get=requests.get(url_get).json()
+    try:
+        client_names=response_url_get["data"][0]["net_data"]
+    except Exception as e:
+        print("解析错误。")
+    if client_names:
+        temp=[]
+        for client_name in client_names:
+            temp.append((client_names[client_name]["client_name"],client_names[client_name]["created_at"]))
+        temp.sort(key=lambda x:x[1])
+        config["client_name"]=temp[-1][0]
     config["cmd"]="show power"
     response=requests.post(url_post,json=config)
     data=response.json()["data"]["cmd_result"]
